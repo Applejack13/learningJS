97d4bf0701937228d65543a13ff0b3bbc0294d97
Vadym <andreeshinvadim13@gmail.com>
2023-03-21 13:53:27 +0200

    Return
    Learned about "return"

--- script.js
+++ script.js
@@ -201,3 +201,30 @@ logger();
 // line function 
 const calc = (a, b) => a + b;
 
+
+const usdCurr = 28;
+const eurCurr = 32;
+const discount = 0.9;
+
+function convert(amount, curr) {
+    return curr * amount;
+}
+
+function promotion(result) {
+    console.log(result * discount);
+}
+
+promotion(convert(500, usdCurr));
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        if (i === 3) return;
+    }
+    console.log('Done');
+}
+
+test();
+
+function doNothing() {}
+console.log(doNothing() === undefined);
