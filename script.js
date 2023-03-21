0309183147a82207aff2365256b91bb40539df6e
Vadym <andreeshinvadim13@gmail.com>
2023-03-21 20:48:16 +0200

    Continue to create an app
    Created new js file and continued to doing an app

--- script.js
+++ script.js
@@ -1,42 +1,4 @@
-// "use sctict";
-
-// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
-
-
-// for (i = 0; i < 2; i++) {
-//     const a = prompt('Один из последних просмотренных фильмов', ''),
-//           b = prompt('На сколько оцените его?', '');
-
-//     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('Done');
-//     } else {
-//         console.log('Error');
-//         i--;
-//     }
-
-// }
-
-// if (personalMovieDB.count < 10) {
-//     console.log('Просмотренно довольно мало фильмов');
-// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//     console.log('Вы классический зритель');
-// } else if (personalMovieDB.count >= 30) {
-//     console.log('Вы - киноман');
-// } else {
-//     console.log('Ошибка!');
-// }
-
-// console.log(personalMovieDB);
-
+"use sctict";
 
 // if (4 == 9) {
 //     console.log('Ok!');
