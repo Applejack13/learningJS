a929eebe4b8323bdf5758ecf71a158e56fe3fa01
Vadym <andreeshinvadim13@gmail.com>
2023-04-02 19:34:01 +0300

    Еvent handlers

--- script.js
+++ script.js
@@ -723,18 +723,58 @@
 
 
 
-function isPrime(n) {
-    if (n <= 1) {
-        return false;
-    }
+// function isPrime(n) {
+//     if (n <= 1) {
+//         return false;
+//     }
 
-    for (let i = 2; i < n; i++) {
-        if (n % i == 0) {
-            return false;
-        }
-    }
+//     for (let i = 2; i < n; i++) {
+//         if (n % i == 0) {
+//             return false;
+//         }
+//     }
 
-    return true;
-}
+//     return true;
+// }
 
-console.log(isPrime(10));
+// console.log(isPrime(10));
+
+
+const btns = document.querySelectorAll('button'),
+      overlay = document.querySelector('.overlay');
+
+
+// btn.onclick = function() {
+//     alert("It's working");
+// };
+
+// btn.addEventListener('mouseenter', () => {
+//     alert("It's working");
+// });
+
+// let i = 0;
+const deleteElement = (e) => {
+    console.log(e.currentTarget);
+    console.log(e.type);
+    // e.target.remove();
+    // alert("It's working");
+    // i++;
+    // if (i == 1) {
+    //     btn.removeEventListener('click', deleteElement);
+    // }
+};
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once: true});
+});
+
+const link = document.querySelector('a');
+
+link.addEventListener('click' , (event) => {
+    event.preventDefault();
+
+    console.log(event.target);
+});
