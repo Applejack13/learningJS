2727049218ae503d424ca46a7700d0c2cc2a6dbb
Vadym <andreeshinvadim13@gmail.com>
2023-04-02 17:14:07 +0300

    Working with the page

--- script.js
+++ script.js
@@ -720,3 +720,21 @@
 // }
 
 // console.log(factorial(3));
+
+
+
+function isPrime(n) {
+    if (n <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPrime(10));
