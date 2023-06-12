9f67f1da39dfc8f9db08256e9b26c4f8c57e8af6
Vadym <andreeshinvadim13@gmail.com>
2023-06-12 21:28:40 +0300

    Learning June

---
+++ learningJune.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} миллисекунд`);
--- script.js
+++ script.js
@@ -1403,3 +1403,14 @@ newWrapper.append(createNewBtns);
   
 //     return result.join(" ");
 //   }
+
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} миллисекунд`);
